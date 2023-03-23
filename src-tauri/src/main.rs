fa0a7a9c5b2b2e2086005c4936a2a296636dba62
Benjamín Navarro <benjamin.navarro@globalhap.com>
2023-03-23 01:05:05 -0300

    conexion con mongodb

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,9 +7,36 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
+use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
+use std::env;
+use std::error::Error;
+use tokio;
+
+#[tokio::main]
+async fn main()-> Result<(), Box<dyn Error>> {
+    
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    let client_uri =
+       env::var("mongodb+srv://tesisAdmin:Afasia123@tesiscluster.0dxugcp.mongodb.net/test?retryWrites=true&w=majority")
+        .expect("You must set the MONGODB_URI environment var!");
+    
+    // A Client is needed to connect to MongoDB:
+    // An extra line of code to work around a DNS issue on Windows:
+    let options =
+       ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
+          .await?;
+    let client = Client::with_options(options)?;
+    
+    // Print the databases in our MongoDB cluster:
+    println!("Databases:");
+    for name in client.list_database_names(None, None).await? {
+       println!("- {}", name);
+    }
+    
+    Ok(())
+
 }
